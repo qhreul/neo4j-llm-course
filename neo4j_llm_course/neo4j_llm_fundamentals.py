801cefd291103139eea7796c65a4efc4b64a04e0
Quentin Reul <Quentin.H.Reul@gmail.com>
2024-03-25 10:51:08 -0500

    Chat LLM + chain

--- neo4j_llm_course/neo4j_llm_fundamentals.py
+++ neo4j_llm_course/neo4j_llm_fundamentals.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 from langchain_community.chat_models.ollama import ChatOllama
 from langchain_core.messages import HumanMessage, SystemMessage
 
@@ -9,13 +11,17 @@ load_dotenv()
 instruction_message = """
 You are a surfer dude, having a conversation about the surf conditions on the beach.
 Respond using surfer slang.
+
+Question: {question}
 """
 
 
 if __name__ == '__main__':
     # create a prompt template
-    instructions = SystemMessage(content=instruction_message)
-    question = HumanMessage(content="What is the weather like?")
+    prompt = PromptTemplate(
+        template=instruction_message,
+        input_variables=["question"]
+    )
 
     # initiate the Ollama client
     chat_llm = ChatOllama(
@@ -24,6 +30,12 @@ if __name__ == '__main__':
         temperature=0.0
     )
 
+    # initiate the LLMChain
+    chat_chain = LLMChain(
+        llm=chat_llm,
+        prompt=prompt
+    )
+
     # execute the LLM
-    response = chat_llm.invoke([instructions, question])
-    print(response.content)
+    response = chat_chain.invoke({"question": "What is the weather like today?"})
+    print(response)

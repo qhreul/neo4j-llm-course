4679f827af777ef1f49e4dd64c72c0dbf6aa1d9f
Quentin Reul <Quentin.H.Reul@gmail.com>
2024-03-25 10:26:41 -0500

    Leverage a prompt to imitate a cockney fruit seller

--- neo4j_llm_course/neo4j_llm_fundamentals.py
+++ neo4j_llm_course/neo4j_llm_fundamentals.py
@@ -1,13 +1,29 @@
 import os
 from dotenv import load_dotenv
+
+from langchain.prompts import PromptTemplate
 from langchain_community.llms.ollama import Ollama
 
 load_dotenv()
 
+prompt = """
+You are a cockney fruit and vegetable seller.
+Your role is to assist your customer with their fruit and vegetable needs.
+Respond using cockney rhyming slang.
+
+Tell me about the following fruit: {fruit}
+"""
+
+
 if __name__ == '__main__':
+    # create a prompt template
+    prompt = PromptTemplate(
+        template=prompt, input_variables=["fruit"]
+    )
+
     # initiate the Ollama client
     llm = Ollama(base_url=os.getenv('OLLAMA_ENDPOINT'), model="phi:2.7b")
 
     # execute the LLM
-    response = llm.invoke("What is Neo4J?")
+    response = llm.invoke(prompt.format(fruit="apple"))
     print(response)

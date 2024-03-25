b2b30a24d0489e354313199b414912543dbed580
Quentin Reul <Quentin.H.Reul@gmail.com>
2024-03-25 11:05:42 -0500

    Introduction of conversation buffer
    Chat history is included in the requests to provide some level of memories

--- neo4j_llm_course/neo4j_llm_fundamentals.py
+++ neo4j_llm_course/neo4j_llm_fundamentals.py
@@ -2,9 +2,9 @@ import os
 from dotenv import load_dotenv
 
 from langchain.chains import LLMChain
+from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 from langchain_community.chat_models.ollama import ChatOllama
-from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
@@ -12,6 +12,7 @@ instruction_message = """
 You are a surfer dude, having a conversation about the surf conditions on the beach.
 Respond using surfer slang.
 
+Chat History: {chat_history}
 Context: {context}
 Question: {question}
 """
@@ -21,7 +22,12 @@ if __name__ == '__main__':
     # create a prompt template
     prompt = PromptTemplate(
         template=instruction_message,
-        input_variables=["context", "question"]
+        input_variables=["chat_history", "context", "question"]
+    )
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        input_key="question",
+        return_messages=True
     )
 
     # initiate the Ollama client
@@ -34,7 +40,8 @@ if __name__ == '__main__':
     # initiate the LLMChain
     chat_chain = LLMChain(
         llm=chat_llm,
-        prompt=prompt
+        prompt=prompt,
+        memory=memory
     )
 
     # providing some context to the LLM - minimal / manual RAG
@@ -52,4 +59,10 @@ if __name__ == '__main__':
         "context": current_weather,
         "question": "What is the weather like today in Watergate Bay?"
     })
-    print(response)
+    print(response["text"])
+
+    response = chat_chain.invoke({
+        "context": current_weather,
+        "question": "Where I am?"
+    })
+    print(response["text"])

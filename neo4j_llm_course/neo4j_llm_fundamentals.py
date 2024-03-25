52465eb3433b97a7ff708565ccc7f03c59c91226
Quentin Reul <Quentin.H.Reul@gmail.com>
2024-03-25 10:48:03 -0500

    Converted to a chat model

--- neo4j_llm_course/neo4j_llm_fundamentals.py
+++ neo4j_llm_course/neo4j_llm_fundamentals.py
@@ -1,41 +1,29 @@
 import os
 from dotenv import load_dotenv
 
-from langchain.chains import LLMChain
-from langchain.prompts import PromptTemplate
-from langchain.schema import StrOutputParser
-from langchain_community.llms.ollama import Ollama
+from langchain_community.chat_models.ollama import ChatOllama
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
-prompt = """
-You are a cockney fruit and vegetable seller.
-Your role is to assist your customer with their fruit and vegetable needs.
-Respond using cockney rhyming slang.
-
-Tell me about the following fruit: {fruit}
+instruction_message = """
+You are a surfer dude, having a conversation about the surf conditions on the beach.
+Respond using surfer slang.
 """
 
 
 if __name__ == '__main__':
     # create a prompt template
-    prompt = PromptTemplate(
-        template=prompt, input_variables=["fruit"]
-    )
+    instructions = SystemMessage(content=instruction_message)
+    question = HumanMessage(content="What is the weather like?")
 
     # initiate the Ollama client
-    llm = Ollama(
+    chat_llm = ChatOllama(
         base_url=os.getenv('OLLAMA_ENDPOINT'),
         model="gemma:7b",
-        temperature=0.0)
-
-    # initiate the LLM chain
-    llm_chain = LLMChain(
-        llm=llm,
-        prompt=prompt,
-        output_parser=StrOutputParser()
+        temperature=0.0
     )
 
     # execute the LLM
-    response = llm_chain.invoke({"fruit": "apple"})
-    print(response)
+    response = chat_llm.invoke([instructions, question])
+    print(response.content)

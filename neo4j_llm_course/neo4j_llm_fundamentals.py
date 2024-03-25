0ee4eaeaf7c07c0b849ec67a2839ee3766f33e0d
Quentin Reul <Quentin.H.Reul@gmail.com>
2024-03-25 10:40:08 -0500

    LLM Chain and output parsers

--- neo4j_llm_course/neo4j_llm_fundamentals.py
+++ neo4j_llm_course/neo4j_llm_fundamentals.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import load_dotenv
 
+from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.schema import StrOutputParser
 from langchain_community.llms.ollama import Ollama
 
 load_dotenv()
@@ -22,8 +24,18 @@ if __name__ == '__main__':
     )
 
     # initiate the Ollama client
-    llm = Ollama(base_url=os.getenv('OLLAMA_ENDPOINT'), model="phi:2.7b")
+    llm = Ollama(
+        base_url=os.getenv('OLLAMA_ENDPOINT'),
+        model="gemma:7b",
+        temperature=0.0)
+
+    # initiate the LLM chain
+    llm_chain = LLMChain(
+        llm=llm,
+        prompt=prompt,
+        output_parser=StrOutputParser()
+    )
 
     # execute the LLM
-    response = llm.invoke(prompt.format(fruit="apple"))
+    response = llm_chain.invoke({"fruit": "apple"})
     print(response)
